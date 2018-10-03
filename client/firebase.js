844b42d9f6d4c9d8083f98198b831ba0c7ea4d0b
Luis Quinones <alexx813@gmail.com>
2018-10-03 07:05:22 -0500

    initial commit

---
+++ client/firebase.js
@@ -0,0 +1,10 @@
+export default Register;
+  var config = {
+    apiKey: "AIzaSyBnwPUU3DQeMWSFLhTPIC_MpYkhuZ1_2UA",
+    authDomain: "slack-clone-e0054.firebaseapp.com",
+    databaseURL: "https://slack-clone-e0054.firebaseio.com",
+    projectId: "slack-clone-e0054",
+    storageBucket: "slack-clone-e0054.appspot.com",
+    messagingSenderId: "377614347143"
+  };
+  firebase.initializeApp(config);
---
+++ client/src/components/Auth/Register.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Register extends React.Component {
+  render() {
+    return <div>Register</div>;
+  }
+}
+
